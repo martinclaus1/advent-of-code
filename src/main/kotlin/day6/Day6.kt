e94c20fe827d9441cd02dd7772a44b102d5276de
Martin Claus <martinclaus@posteo.de>
2024-12-10 08:20:46 +0100

    adjust day 6

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -4,11 +4,7 @@ import dev.martinclaus.Day
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 import kotlinx.coroutines.withTimeoutOrNull
 
 
@@ -22,6 +18,22 @@ class Day6 : Day {
     override fun partI(input: String): Int {
         val lines = input.lines().map { it.toList() }
         return getVisitedCount(lines) ?: 0
+    }
+
+    override fun partII(input: String): Int = runBlocking {
+        val lines = input.lines().map { it.toList() }
+
+        val deferredResults = lines.mapIndexed { y, row ->
+            List(row.size) { x ->
+                CoroutineScope(Dispatchers.IO.limitedParallelism(10)).async {
+                    getVisitedCount(lines.modifyCell(Point(x, y), 'O'))
+                }
+            }
+        }.flatten()
+
+        deferredResults.count {
+            withTimeoutOrNull(50) { it.await() } == null
+        }
     }
 
     private fun getVisitedCount(lines: List<List<Char>>): Int? {
@@ -70,22 +82,6 @@ class Day6 : Day {
             if (x == point.x && y == point.y && this[y][x] != '^') value else c
         }
     }
-
-    override fun partII(input: String): Int = runBlocking {
-        val lines = input.lines().map { it.toList() }
-
-        val deferredResults = lines.mapIndexed { y, row ->
-            List(row.size) { x ->
-                CoroutineScope(Dispatchers.IO.limitedParallelism(10)).async {
-                    getVisitedCount(lines.modifyCell(Point(x, y), 'O'))
-                }
-            }
-        }.flatten()
-
-        deferredResults.count {
-            withTimeoutOrNull(50) { it.await() } == null
-        }
-    }
 }
 
 data class Point(val x: Int, val y: Int)
@@ -96,6 +92,3 @@ enum class Direction(val y: Int, val x: Int) {
     LEFT(0, -1),
     RIGHT(0, 1),
 }
-
-
-
