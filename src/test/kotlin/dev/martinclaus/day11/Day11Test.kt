3bd196efa067f64eb58958273cae88795f4ce986
Martin Claus <martinclaus@posteo.de>
2024-12-15 11:24:34 +0100

    add day 11

---
+++ src/test/kotlin/dev/martinclaus/day11/Day11Test.kt
@@ -0,0 +1,52 @@
+package dev.martinclaus.day11
+
+import dev.martinclaus.dev.martinclaus.readText
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.ArgumentsProvider
+import org.junit.jupiter.params.provider.ArgumentsSource
+import java.util.stream.Stream
+import java.util.stream.Stream.of
+import kotlin.test.assertEquals
+
+class Day11Test {
+    private val sut = Day11()
+
+    @ParameterizedTest
+    @ArgumentsSource(PartIArgumentsProvider::class)
+    fun checkPartI(input: String, expected: Long) {
+        val result = sut.partI(input)
+        assertEquals(expected, result)
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(PartIIArgumentsProvider::class)
+    fun checkPartII(input: String, expected: Long) {
+        val result = sut.partII(input)
+        assertEquals(expected, result)
+    }
+
+    class PartIArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments>? = of(
+            Arguments.of("125 17", 55312),
+            Arguments.of(Day11.INPUT_FILE.readText(), 203457)
+        )
+    }
+
+    class PartIIArgumentsProvider : ArgumentsProvider {
+        override fun provideArguments(context: ExtensionContext): Stream<out Arguments>? = of(
+            Arguments.of("""
+                89010123
+                78121874
+                87430965
+                96549874
+                45678903
+                32019012
+                01329801
+                10456732
+            """.trimIndent(), 65601038650482),
+            Arguments.of(Day11.INPUT_FILE.readText(), 241394363462435)
+        )
+    }
+}
