7448b9e06a84cd667a45ac3d2a8be35958645479
Martin Claus <martinclaus@posteo.de>
2024-12-23 19:38:41 +0100

    add dirty day23

--- src/main/kotlin/dev/martinclaus/y2024/day23/Day23.kt
+++ src/main/kotlin/dev/martinclaus/y2024/day23/Day23.kt
@@ -9,21 +9,10 @@ class Day23 {
             listOf(a to b, b to a)
         }.groupBy { it.first }.mapValues { it.value.map { it.second }.toSet() }
 
-        val triples = connections.entries.flatMap { (key, connection) ->
-            combineElements(connection.toList()).filter { (a, b) ->
-                connections[a]?.contains(b) == true && connections[b]?.contains(
-                    a
-                ) == true
-            }.map { (a, b) -> listOf(key, a, b).sorted() }
-        }.filter { it.any { it.startsWith("t") } }.toSet()
+        return BronKerbosch2(connections).largestClique().asSequence().filter { it.size >= 3 }
+            .flatMap { it.toList().combinations(3) }.map { it.sorted() }.filter { it.any { it.startsWith("t") } }
+            .toSet().size.toLong()
 
-        return triples.size.toLong()
-    }
-
-    fun <T> combineElements(list: List<T>): List<Pair<T, T>> {
-        return list.flatMapIndexed { index, element ->
-            list.drop(index + 1).map { Pair(element, it) }
-        }
     }
 
     fun partII(input: String): String = BronKerbosch(input.safeLines().flatMap {
@@ -64,3 +53,51 @@ class BronKerbosch<T>(private val neighbors: Map<T, Set<T>>) {
         }
     }
 }
+
+class BronKerbosch2<T>(private val neighbors: Map<T, Set<T>>) {
+
+    private var bestR = mutableListOf<Set<T>>()
+
+    fun largestClique(): List<Set<T>> {
+        execute(neighbors.keys)
+        return bestR
+    }
+
+    private fun execute(
+        p: Set<T>,
+        r: Set<T> = emptySet(),
+        x: Set<T> = emptySet()
+    ) {
+        if (p.isEmpty() && x.isEmpty()) {
+            // We have found a potential best R value, compare it to the best so far.
+            bestR.add(r)
+        } else {
+            val mostNeighborsOfPandX: T = (p + x).maxBy { neighbors.getValue(it).size }!!
+            val pWithoutNeighbors = p.minus(neighbors[mostNeighborsOfPandX]!!)
+            pWithoutNeighbors.forEach { v ->
+                val neighborsOfV = neighbors[v]!!
+                execute(
+                    p.intersect(neighborsOfV),
+                    r + v,
+                    x.intersect(neighborsOfV)
+                )
+            }
+        }
+    }
+}
+
+fun <T> List<T>.combinations(size: Int): List<List<T>> {
+    if (size <= 0) return listOf(emptyList())
+    if (size > this.size) return emptyList()
+    if (size == this.size) return listOf(this)
+    if (size == 1) return this.map { listOf(it) }
+
+    val combinations = mutableListOf<List<T>>()
+    val rest = this.drop(1)
+    rest.combinations(size - 1).forEach { combination ->
+        combinations.add(listOf(this[0]) + combination)
+    }
+    combinations += rest.combinations(size)
+    return combinations
+}
+
